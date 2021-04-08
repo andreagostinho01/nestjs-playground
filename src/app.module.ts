7866cc0b965a3f1a15288ec506954c0f01925d60
André <46069931+andreagostinho01@users.noreply.github.com>
2021-04-08 09:55:08 -0300

    add task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksController } from './tasks/tasks.controller';
-import { TaskService } from './tasks/shared/task.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TasksController],
-  providers: [AppService, TaskService],
+  imports: [TasksModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
