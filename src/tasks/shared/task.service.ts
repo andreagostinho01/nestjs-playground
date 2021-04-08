dd0cd22b6663a09c4655034fba61ac38f180230e
André <46069931+andreagostinho01@users.noreply.github.com>
2021-04-08 13:39:42 -0300

    add mongoose lib

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './task';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  tasks: Task[] = [
+    { id: 1, description: 'Tarefa 1', completed: true },
+    { id: 2, description: 'Tarefa 2', completed: true },
+    { id: 3, description: 'Tarefa 3', completed: false },
+    { id: 4, description: 'Tarefa 4', completed: true },
+    { id: 5, description: 'Tarefa 5', completed: false },
+  ];
+
+  getAll(): Task[] {
+    return this.tasks;
+  }
+
+  getById(id: number) {
+    return this.tasks.find((item) => item.id === id);
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Task } from './shared/task';
+import { TaskService } from './shared/task.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TaskService) {}
+
+  @Get()
+  async getAll(): Promise<Task[]> {
+    return this.taskService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.taskService.getById(id);
+  }
+}
